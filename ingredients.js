a3c5cac4763d21db7a018b7758f1e09ef0981e2b
Rohan N <rohannagendra36@gmail.com>
2021-06-06 21:47:00 +0000

    array excercises

---
+++ ingredients.js
@@ -0,0 +1,5 @@
+var ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+
+for (var i = ingredients.length - 1; i >= 0; i--) {
+  console.log(ingredients[i]);
+}
